809fc95621a124b58b4a3ebbdf52aaefb90a3d7b
Andreas Wieland <and.wieland@gmail.com>
2012-11-13 17:52:27 +0100

    added MapOverlay class
    Map_detail_dialog:
    added: picture to layout
    
    MapItemnizedOverlay:
    changed: OverlayItem to MapoverlayItem
    implemented: buildDialog

--- helpme.demo/src/com/android/helpme/demo/overlay/MapItemnizedOverlay.java
+++ helpme.demo/src/com/android/helpme/demo/overlay/MapItemnizedOverlay.java
@@ -5,10 +5,20 @@ package com.android.helpme.demo.overlay;
 
 import java.util.ArrayList;
 
+import org.json.simple.JSONObject;
+
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.text.Html;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.android.helpme.demo.R;
+import com.android.helpme.demo.interfaces.UserInterface;
+import com.android.helpme.demo.utils.Task;
+import com.android.helpme.demo.utils.User;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapView;
@@ -19,23 +29,23 @@ import com.google.android.maps.OverlayItem;
  *
  */
 public class MapItemnizedOverlay extends ItemizedOverlay<OverlayItem> {
-	private ArrayList<OverlayItem> items;
+	private ArrayList<MapOverlayItem> items;
 	private Context context;
 
 	public MapItemnizedOverlay(Drawable drawable) {
 		super(boundCenterBottom(drawable));
-		items = new ArrayList<OverlayItem>();
+		items = new ArrayList<MapOverlayItem>();
 		populate();
 	}
 
 	public MapItemnizedOverlay(Drawable defaultMarker, Context context) {
 		super(boundCenterBottom(defaultMarker));
 		this.context = context;
-		items = new ArrayList<OverlayItem>();
+		items = new ArrayList<MapOverlayItem>();
 		populate();
 	}
 
-	public void addOverlay(OverlayItem overlay) {
+	public void addOverlay(MapOverlayItem overlay) {
 		items.add(overlay);
 		populate();
 	}
@@ -50,24 +60,46 @@ public class MapItemnizedOverlay extends ItemizedOverlay<OverlayItem> {
 		return items.size();
 	}
 	
-	public ArrayList<OverlayItem> getItems() {
+	public ArrayList<MapOverlayItem> getItems() {
 		return items;
 	}
 	
 	@Override
 	protected boolean onTap(int index) {
-		OverlayItem item = items.get(index);
+		MapOverlayItem item = items.get(index);
 		if (item != null) {
-			AlertDialog.Builder dialog = new AlertDialog.Builder(context);
-			dialog.setTitle(item.getTitle());
-			dialog.setMessage(item.getSnippet());
-			dialog.show();
+			buildDialog(item).show();
 			return true;
 		}
 		return false;
 	}
 
-	public void removeItem(OverlayItem item) {
+	public void removeItem(MapOverlayItem item) {
 		items.remove(item);
+	}
+	
+	private Dialog buildDialog(MapOverlayItem item) {
+		JSONObject object = item.getJsonObject();
+		UserInterface userInterface = new User((JSONObject) object.get(Task.USER));
+		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+
+		Dialog dialog = dialogBuilder.show();
+		//		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		dialog.setContentView(R.layout.map_detail_dialog);
+
+		ImageView imageView; TextView text;
+		imageView = (ImageView) dialog.findViewById(R.id.map_pic);
+		imageView.setImageDrawable(item.getDrawable());
+		
+		text = (TextView) dialog.findViewById(R.id.map_name);
+		text.setText(Html.fromHtml(context.getText(R.string.dialog_name) + userInterface.getName()));
+		
+		text = (TextView) dialog.findViewById(R.id.history_age);
+		text.setText(Html.fromHtml(context.getString(R.string.dialog_age) + new Integer(userInterface.getAge()).toString()));
+
+		text = (TextView) dialog.findViewById(R.id.history_gender);
+		text.setText(Html.fromHtml(context.getString(R.string.dialog_gender) + userInterface.getGender() ));
+
+		return dialog;
 	}
 }
---
+++ helpme.demo/src/com/android/helpme/demo/overlay/MapOverlayItem.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.android.helpme.demo.overlay;
+
+import org.json.simple.JSONObject;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.OverlayItem;
+
+/**
+ * @author Andreas Wieland
+ *
+ */
+public class MapOverlayItem extends OverlayItem {
+
+	private JSONObject jsonObject;
+	private Drawable drawable;
+	/**
+	 * @param point
+	 * @param title
+	 * @param snippet
+	 */
+	public MapOverlayItem(GeoPoint point, String title, String snippet) {
+		super(point, title, snippet);
+		jsonObject = null;
+		drawable = null;
+	}
+
+	public MapOverlayItem(GeoPoint point, String title, String snippet, JSONObject jsonObject, Drawable drawable) {
+		super(point, title, snippet);
+		this.jsonObject = jsonObject;
+		this.drawable = drawable;
+	}
+
+	public JSONObject getJsonObject() {
+		return jsonObject;
+	}
+
+	public void setJsonObject(JSONObject jsonObject) {
+		this.jsonObject = jsonObject;
+	}
+
+	public Drawable getDrawable() {
+		return drawable;
+	}
+
+	public void setDrawable(Drawable drawable) {
+		this.drawable = drawable;
+	}
+}
