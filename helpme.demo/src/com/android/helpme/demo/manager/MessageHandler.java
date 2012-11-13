0ac092316b7d12623d6a1ceea058ca107e6f2457
Martin Majewski <mail@martinmajewski.net>
2012-11-13 09:23:19 +0100

    lowered the min- and target-sdk api number to 14
    Prevented a NullPointerException at the MessageHandler class. -> Should be reviewed!

--- helpme.demo/src/com/android/helpme/demo/manager/MessageHandler.java
+++ helpme.demo/src/com/android/helpme/demo/manager/MessageHandler.java
@@ -1,15 +1,16 @@
 package com.android.helpme.demo.manager;
 
 import java.util.ArrayList;
+
 import com.android.helpme.demo.exceptions.UnkownMessageType;
 import com.android.helpme.demo.exceptions.WrongObjectType;
 import com.android.helpme.demo.interfaces.DrawManagerInterface;
+import com.android.helpme.demo.interfaces.DrawManagerInterface.DRAWMANAGER_TYPE;
 import com.android.helpme.demo.interfaces.HistoryManagerInterface;
 import com.android.helpme.demo.interfaces.MessageHandlerInterface;
 import com.android.helpme.demo.interfaces.PositionManagerInterface;
 import com.android.helpme.demo.interfaces.RabbitMQManagerInterface;
 import com.android.helpme.demo.interfaces.UserManagerInterface;
-import com.android.helpme.demo.interfaces.DrawManagerInterface.DRAWMANAGER_TYPE;
 import com.android.helpme.demo.messagesystem.AbstractMessageSystem;
 import com.android.helpme.demo.messagesystem.InAppMessage;
 import com.android.helpme.demo.utils.User;
@@ -35,27 +36,29 @@ public abstract class MessageHandler extends AbstractMessageSystem implements Me
 	 * @param message
 	 */
 	protected void handlePositionMessage(InAppMessage message) {
-		switch (message.getType()) {
-		case LOCATION:
-			if (!(message.getObject() instanceof Position)) {
-				fireError(new WrongObjectType(message.getObject(), Position.class));
-				return;
-			}
-			Position position = (Position) message.getObject();
-			userManagerInterface.thisUser().updatePosition(position);
+		switch(message.getType()) {
+			case LOCATION:
+				if(!(message.getObject() instanceof Position)) {
+					fireError(new WrongObjectType(message.getObject(), Position.class));
+					return;
+				}
+				Position position = (Position) message.getObject();
+				if(position != null) {
+					userManagerInterface.thisUser().updatePosition(position);
+				}
+				if(getDrawManager(DRAWMANAGER_TYPE.MAP) != null) {
+					getDrawManager(DRAWMANAGER_TYPE.MAP).drawThis(userManagerInterface.getThisUser());
+				}
 
-			if (getDrawManager(DRAWMANAGER_TYPE.MAP) != null) {
-				getDrawManager(DRAWMANAGER_TYPE.MAP).drawThis(userManagerInterface.getThisUser());
-			}
+				if(historyManagerInterface.getTask() != null) {
+					historyManagerInterface.getTask().sendPosition(position);
+				}
 
-			if (historyManagerInterface.getTask() != null) {
-				historyManagerInterface.getTask().sendPosition(position);
-			}
-			break;
+				break;
 
-		default:
-			fireError(new UnkownMessageType());
-			break;
+			default:
+				fireError(new UnkownMessageType());
+				break;
 		}
 	}
 
@@ -65,45 +68,45 @@ public abstract class MessageHandler extends AbstractMessageSystem implements Me
 	 * @param message
 	 */
 	protected void handleRabbitMQMessages(InAppMessage message) {
-		switch (message.getType()) {
-		case UNBOUND_FROM_SERVICE:
-			// TODO
-			break;
-		case BOUND_TO_SERVICE:
-			run(rabbitMQManagerInterface.connect());
-			break;
+		switch(message.getType()) {
+			case UNBOUND_FROM_SERVICE:
+				// TODO
+				break;
+			case BOUND_TO_SERVICE:
+				run(rabbitMQManagerInterface.connect());
+				break;
 
-		case CONNECTED:
-			run(rabbitMQManagerInterface.subscribeToMainChannel());
-			break;
+			case CONNECTED:
+				run(rabbitMQManagerInterface.subscribeToMainChannel());
+				break;
 
-		case RECEIVED_DATA:
-			if (!(message.getObject() instanceof User)) {
-				fireError(new WrongObjectType(message.getObject(), User.class));
-				return;
-			}
-			User user = (User) message.getObject();
-
-			if (!UserManager.getInstance().isUserSet()) {
-				return;
-			}
-
-			if (!user.getId().equalsIgnoreCase(userManagerInterface.getThisUser().getId())) {
-
-				if (userManagerInterface.getThisUser().isHelper()) {
-					handleIncomingUserAsHelper(user);
-				} else {
-					if (user.isHelper()) {
-						handleIncomingUserAsHelperSeeker(user);
-					}
-
+			case RECEIVED_DATA:
+				if(!(message.getObject() instanceof User)) {
+					fireError(new WrongObjectType(message.getObject(), User.class));
+					return;
 				}
-			}
-			break;
+				User user = (User) message.getObject();
 
-		default:
-			fireError(new UnkownMessageType());
-			break;
+				if(!UserManager.getInstance().isUserSet()) {
+					return;
+				}
+
+				if(!user.getId().equalsIgnoreCase(userManagerInterface.getThisUser().getId())) {
+
+					if(userManagerInterface.getThisUser().isHelper()) {
+						handleIncomingUserAsHelper(user);
+					} else {
+						if(user.isHelper()) {
+							handleIncomingUserAsHelperSeeker(user);
+						}
+
+					}
+				}
+				break;
+
+			default:
+				fireError(new UnkownMessageType());
+				break;
 		}
 	}
 
@@ -114,14 +117,14 @@ public abstract class MessageHandler extends AbstractMessageSystem implements Me
 	 * @param incomingUser
 	 */
 	private void handleIncomingUserAsHelper(User incomingUser) {
-		if (userManagerInterface.addUser(incomingUser)) {
+		if(userManagerInterface.addUser(incomingUser)) {
 			run(rabbitMQManagerInterface.showNotification(incomingUser));
 		}
 
-		if (historyManagerInterface.getTask() != null && getDrawManager(DRAWMANAGER_TYPE.MAP) != null) {
+		if(historyManagerInterface.getTask() != null && getDrawManager(DRAWMANAGER_TYPE.MAP) != null) {
 			historyManagerInterface.getTask().updatePosition(incomingUser);
 
-			if ( historyManagerInterface.getTask().isUserInShortDistance()) {
+			if(historyManagerInterface.getTask().isUserInShortDistance()) {
 				getDrawManager(DRAWMANAGER_TYPE.MAP).drawThis(historyManagerInterface.getTask());
 
 			} else {
@@ -129,7 +132,7 @@ public abstract class MessageHandler extends AbstractMessageSystem implements Me
 			}
 		}
 
-		if (getDrawManager(DRAWMANAGER_TYPE.HELPER) != null) {
+		if(getDrawManager(DRAWMANAGER_TYPE.HELPER) != null) {
 			getDrawManager(DRAWMANAGER_TYPE.HELPER).drawThis(incomingUser);
 		}
 
@@ -143,19 +146,19 @@ public abstract class MessageHandler extends AbstractMessageSystem implements Me
 	 */
 	private void handleIncomingUserAsHelperSeeker(User incomingUser) {
 		userManagerInterface.addUser(incomingUser);
-		if (historyManagerInterface.getTask() != null) {
+		if(historyManagerInterface.getTask() != null) {
 			historyManagerInterface.getTask().updatePosition(incomingUser);
 		}
 
-		if (getDrawManager(DRAWMANAGER_TYPE.HELPERCOMMING) != null) {
-			if (historyManagerInterface.getTask().isUserInShortDistance()) {
+		if(getDrawManager(DRAWMANAGER_TYPE.HELPERCOMMING) != null) {
+			if(historyManagerInterface.getTask().isUserInShortDistance()) {
 				getDrawManager(DRAWMANAGER_TYPE.HELPERCOMMING).drawThis(historyManagerInterface.getTask());
 			} else {
 				getDrawManager(DRAWMANAGER_TYPE.HELPERCOMMING).drawThis(incomingUser);
 			}
 
 		} else {
-			if (getDrawManager(DRAWMANAGER_TYPE.SEEKER) != null) {
+			if(getDrawManager(DRAWMANAGER_TYPE.SEEKER) != null) {
 				getDrawManager(DRAWMANAGER_TYPE.SEEKER).drawThis(incomingUser);
 			}
 
@@ -163,26 +166,26 @@ public abstract class MessageHandler extends AbstractMessageSystem implements Me
 	}
 
 	protected void handleHistoryMessages(InAppMessage message) {
-		switch (message.getType()) {
-		case TIMEOUT:
-			historyManagerInterface.stopTask();
-			getDrawManager(DRAWMANAGER_TYPE.SEEKER).drawThis(message.getObject());
-			break;
-		case LOADED:
-			if (getDrawManager(DRAWMANAGER_TYPE.HISTORY) != null) {
-				getDrawManager(DRAWMANAGER_TYPE.HISTORY).drawThis(message.getObject());
-			}
-			break;
+		switch(message.getType()) {
+			case TIMEOUT:
+				historyManagerInterface.stopTask();
+				getDrawManager(DRAWMANAGER_TYPE.SEEKER).drawThis(message.getObject());
+				break;
+			case LOADED:
+				if(getDrawManager(DRAWMANAGER_TYPE.HISTORY) != null) {
+					getDrawManager(DRAWMANAGER_TYPE.HISTORY).drawThis(message.getObject());
+				}
+				break;
 
-		case HISTORY:
-			if (getDrawManager(DRAWMANAGER_TYPE.HISTORY) != null) {
-				getDrawManager(DRAWMANAGER_TYPE.HISTORY).drawThis(message.getObject());
-			}
-			break;
+			case HISTORY:
+				if(getDrawManager(DRAWMANAGER_TYPE.HISTORY) != null) {
+					getDrawManager(DRAWMANAGER_TYPE.HISTORY).drawThis(message.getObject());
+				}
+				break;
 
-		default:
-			fireError(new UnkownMessageType());
-			break;
+			default:
+				fireError(new UnkownMessageType());
+				break;
 		}
 	}
 
@@ -192,34 +195,34 @@ public abstract class MessageHandler extends AbstractMessageSystem implements Me
 	 * @param message
 	 */
 	protected void handleUserMessages(InAppMessage message) {
-		switch (message.getType()) {
-		case LOADED:
-			if (getDrawManager(DRAWMANAGER_TYPE.SWITCHER) != null) {
-				getDrawManager(DRAWMANAGER_TYPE.SWITCHER).drawThis(message.getObject());
-			}
+		switch(message.getType()) {
+			case LOADED:
+				if(getDrawManager(DRAWMANAGER_TYPE.SWITCHER) != null) {
+					getDrawManager(DRAWMANAGER_TYPE.SWITCHER).drawThis(message.getObject());
+				}
 
-			break;
-		case RECEIVED_DATA:
-			if (!(message.getObject() instanceof ArrayList<?>)) {
-				fireError(new WrongObjectType(message.getObject(), ArrayList.class));
-				return;
-			}
+				break;
+			case RECEIVED_DATA:
+				if(!(message.getObject() instanceof ArrayList<?>)) {
+					fireError(new WrongObjectType(message.getObject(), ArrayList.class));
+					return;
+				}
 
-			if (getDrawManager(DRAWMANAGER_TYPE.LOGIN) != null) {
-				getDrawManager(DRAWMANAGER_TYPE.LOGIN).drawThis(message.getObject());
-			}
-			break;
+				if(getDrawManager(DRAWMANAGER_TYPE.LOGIN) != null) {
+					getDrawManager(DRAWMANAGER_TYPE.LOGIN).drawThis(message.getObject());
+				}
+				break;
 
-		case CHANGED:
-			if (getDrawManager(DRAWMANAGER_TYPE.HELPER) != null) {
-				getDrawManager(DRAWMANAGER_TYPE.HELPER).drawThis(message.getObject());
-			}
+			case CHANGED:
+				if(getDrawManager(DRAWMANAGER_TYPE.HELPER) != null) {
+					getDrawManager(DRAWMANAGER_TYPE.HELPER).drawThis(message.getObject());
+				}
 
-			break;
+				break;
 
-		default:
-			fireError(new UnkownMessageType());
-			break;
+			default:
+				fireError(new UnkownMessageType());
+				break;
 		}
 	}
 }
